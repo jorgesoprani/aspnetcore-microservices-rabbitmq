f41d036d6ab92936f8f15a7e0e2fe307037b82fa
Jorge Luiz Baldan Soprani <jorgesoprani@gmail.com>
2020-02-19 22:38:25 -0300

    Added IOC container project

---
+++ src/MicroRabbitMQ.Infrastructure.IOC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbitMQ.Core.Domain.Bus;
+using MicroRabbitMQ.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbitMQ.Infrastructure.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
