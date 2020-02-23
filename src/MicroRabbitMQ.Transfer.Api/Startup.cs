5ec1f1efa5fe27eb20bb7c3600877f43eca0bc1e
Jorge Luis Baldan Soprani <jorgesoprani@gmail.com>
2020-02-23 11:16:42 -0300

    Transfer microservice DbContext setup

--- src/MicroRabbitMQ.Transfer.Api/Startup.cs
+++ src/MicroRabbitMQ.Transfer.Api/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using MicroRabbitMQ.Infrastructure.IOC;
+using MicroRabbitMQ.Transfer.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +29,31 @@ namespace MicroRabbitMQ.Transfer.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TransferDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TransferDbContext"));
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Transfer Microservice",
+                    Version = "v1"
+                });
+            });
+
+            //services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddMediatR(typeof(Startup));
+
             services.AddControllers();
+
+            RegisterServices(services);
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +69,12 @@ namespace MicroRabbitMQ.Transfer.Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transfer Microservice v1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
---
+++ src/MicroRabbitMQ.Transfer.Infrastructure/Data/TransferDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbitMQ.Transfer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbitMQ.Transfer.Infrastructure.Data
+{
+    public class TransferDbContext : DbContext
+    {
+        public TransferDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<TransferLog> TransferLogs { get; set; }
+    }
+}
